e69991a4de7de2c2f66fa8de16ef166789e98cc5
Paelion <aelion.pauline1@gmail.com>
2021-02-05 15:24:51 +0100

    [UPDATE] btn link

---
+++ src/Components/Home/Intro.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import '../../Sass/components/_intro.scss';
+
+const Introduction = ({ title, desc, link, label }) => {
+    return (
+        <Router>
+            <div className="home__intro">
+                <h2>{title}</h2>
+                <p>{desc}</p>
+                <div className="btn__wave">
+                    <Link to={link} className="btn__wrapper">
+                        <div className="btn__label">
+                            {label}
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </Router>
+    )
+}
+
+export default Introduction;
